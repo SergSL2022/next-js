5e8f14c069fd44959675b08fb55c1b040ac335fe
Serhiy Slipchuk <slipchuks@ukr.net>
2024-03-08 15:19:20 +0200

    Update home page

--- app/page.js
+++ app/page.js
@@ -39,7 +39,7 @@ export default function Home() {
         />
       </div>
 
-      <h1>TEST2 TEST2 TEST2</h1>
+      <h1>TESTING JENKINS DEPLOYMENT</h1>
       <p>test test test test</p>
     </main>
   );
